4015712cbe8cd7a4130e68b6bb71ecaf3a3da599
AdamFenton <adamfenton96@live.co.uk>
2022-10-31 20:32:14 +0000

    Added functionality to select csv file containing ISBNs when building a new database or adding a batch scan

--- main.py
+++ main.py
@@ -2,17 +2,21 @@ from barcode_lookup import *
 import os
 import pandas as pd
 from pathlib import Path # Required to create thumbnail directory
+import easygui as g
+#
 
+#
 collection_exists = os.path.isfile('%s/book_collection.csv' % os.getcwd())
 
 if collection_exists == True:
-    # If the library catalogue already exists, append to it
     df_collection = pd.read_csv('book_collection.csv')
-    single_lookup(9780345806307,df_collection)
-#     batch_lookup(df_collection)
+    # batch_lookup(df_collection)
+    # format_collection(df_collection)
 
 else:
-    # No library created yet so make a blank dataframe for the lookup function
-    # to append to
-    df_collection = pd.DataFrame(columns = ['title','author','published','pages','genre'])
-    batch_lookup(df_collection)
+    title = 'Select CSV file containing ISBN scans'
+    filename = g.fileopenbox(title)
+    df_collection = pd.DataFrame(columns = ['Title','Author','Date Published','Pages','Genre'])
+    batch_lookup(df_collection,filename)
+
+format_collection(df_collection)
