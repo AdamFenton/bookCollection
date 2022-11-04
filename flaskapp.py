7e44aa1fc763cd0d73e5542b06de44f2afedc3ee
AdamFenton <adamfenton96@live.co.uk>
2022-11-04 10:50:37 +0000

    Working on linking out to pages when you click on a book - I wanted to do modals but that is quite complicated...

--- flaskapp.py
+++ flaskapp.py
@@ -5,5 +5,12 @@ cwd = os.getcwd()
 @app.route('/')
 def home():
    return render_template('grid.html')
+
+@app.route('/my-link/')
+def my_link():
+  print ('I got clicked!')
+
+  return 'Click.'
+
 if __name__ == '__main__':
    app.run()
